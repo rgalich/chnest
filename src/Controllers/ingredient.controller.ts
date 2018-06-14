9421a55a4c2c5d095f0911510100a925db5b5137
remi <remigalichon@gmail.com>
2018-06-14 20:49:10 +0200

    init

---
+++ src/Controllers/ingredient.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('ingredient')
+export class IngredientController {
+    
+}
---
+++ src/Repositories/ingredientRepository.ts
@@ -0,0 +1,6 @@
+import {Repository} from "typeorm";
+import {Ingredient} from "../Models/Ingredient";
+
+export class IngredientRepository extends Repository<Ingredient>{
+
+}
